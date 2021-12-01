5da60d8aa8d71f2ab7cc48503168e8d105ae8ed9
Kisalay <kisalaykisu@gmail.com>
2021-12-01 16:23:05 +0530

    Add files via upload

---
+++ elegantframework_2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='elegantcasing',
+    version='1.0.1',
+    description='Change your text to title-case',
+    author='Kisalay',
+    author_email='kisalaykisu@gmail.com',
+    url='https://github.com/Kisalaykisu/Pytest'
+)
